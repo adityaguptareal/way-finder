7ecbfa4a7dd5287794d88a2afb1c876978e69efa
22Rupesh <rovertrupesh@gmail.com>
2025-08-11 23:08:24 +0530

    Some changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 
-// import React from 'react';
-// import Button from './Button';
-// import ToggleSwitch from './ToggleSwitch';
-// import { FiRotateCcw } from 'react-icons/fi';
-// import GroovifiFilters from './GroovifiFilters';
-
-// const App = () => {
-//   return (
-//     <div className="flex flex-col gap-2 items-center justify-center h-screen bg-gray-900 space-y-6 gap-6">
-//       <Button label="Reset" icon={<FiRotateCcw />} variant="gray" />
-//       <Button label="Save playlist" variant="green" />
-//       <ToggleSwitch />
-//       <GroovifiFilters/>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 
 import React from 'react';
 import Button from './Button';
 import ToggleSwitch from './ToggleSwitch';
 import { FiRotateCcw } from 'react-icons/fi';
-// import GroovifiFilters from './GroovifiFilters';
+;
 
 const App = () => {
   return (
@@ -32,7 +12,7 @@ const App = () => {
       <Button label="Reset" icon={<FiRotateCcw />} variant="gray" />
       <Button label="Save playlist" variant="green" />
       <ToggleSwitch />
-      {/* <GroovifiFilters /> */}
+
     </div>
   );
 };
