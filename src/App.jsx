7c9660e3d6d23ad3c979f3688179a96bedb9f461
22Rupesh <rovertrupesh@gmail.com>
2025-08-12 12:16:43 +0530

    Add Row CSS file of App.jsx and Button.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-
 import React from 'react';
 import Button from './Button';
 import ToggleSwitch from './ToggleSwitch';
 import { FiRotateCcw } from 'react-icons/fi';
-;
+import './App.css';
 
 const App = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 space-y-6">
+    <div className="app-container">
       <Button label="Reset" icon={<FiRotateCcw />} variant="gray" />
       <Button label="Save playlist" variant="green" />
       <ToggleSwitch />
@@ -18,3 +16,5 @@ const App = () => {
 };
 
 export default App;
+
+
